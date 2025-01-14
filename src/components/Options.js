49cd59f488d78f90432fe701c201d0ccc602d8e7
Ayleen <ayleenpiteotarpy@gmail.com>
2025-01-14 15:18:55 -0800

    add section title with font

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,18 +1,11 @@
 import '../styles/Options.css';
-import Styles from '../styles/Styles.module.css';
-import SelectAnimalButton from './SelectAnimalButton';
-import BearImage from '../assets/Bear.png';
-import BunnyImage from '../assets/Bunny.png';
-import CatImage from '../assets/Cat.png';
+import SelectAnimalSection from './SelectAnimalSection';
 
 function Options() {
   return(
     <header className="options-window">
-      <div className={Styles.animalButtons}>
-        <SelectAnimalButton image={BearImage} />
-        <SelectAnimalButton image={BunnyImage} />
-        <SelectAnimalButton image={CatImage} />
-      </div>
+      <div className="section-title">animal</div>
+      <SelectAnimalSection />
     </header>
   );
 }
